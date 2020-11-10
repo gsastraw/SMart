2baff473f94b2c316e6e3a7b8979c26396134573
Mislav Milicevic <mislav.milicevic22@gmail.com>
2020-11-10 13:07:51 +0100

    Initial commit

---
+++ src/main/java/se/gu/smart/Main.java
@@ -0,0 +1,8 @@
+package se.gu.smart;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/se/gu/smart/PlaceholderTest.java
@@ -0,0 +1,13 @@
+package se.gu.smart;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public final class PlaceholderTest {
+
+    @Test
+    @Disabled
+    public void test() {
+
+    }
+}
