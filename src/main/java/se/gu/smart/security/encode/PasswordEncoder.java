f41ac1156a3f83bc70b3ece473abaf23c8100d3b
Mislav Milicevic <mislav.milicevic22@gmail.com>
2020-11-23 22:33:40 +0100

    Return salt with password

--- src/main/java/se/gu/smart/security/encode/PasswordEncoder.java
+++ src/main/java/se/gu/smart/security/encode/PasswordEncoder.java
@@ -1,10 +1,32 @@
 package se.gu.smart.security.encode;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.Optional;
+
 public interface PasswordEncoder {
 
-    String encodePassword(String password);
+    Optional<EncodeResult> encodePassword(String password);
     
     static PasswordEncoder getDefaultEncoder() {
         return new Pbkdf2PasswordEncoder();
+    }
+
+    class EncodeResult {
+        private final String password;
+        private final byte[] salt;
+
+        public EncodeResult(String password, byte[] salt) {
+            this.password = requireNonNull(password);
+            this.salt = salt;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public byte[] getSalt() {
+            return salt;
+        }
     }
 }
--- src/main/java/se/gu/smart/security/encode/Pbkdf2PasswordEncoder.java
+++ src/main/java/se/gu/smart/security/encode/Pbkdf2PasswordEncoder.java
@@ -1,12 +1,11 @@
 package se.gu.smart.security.encode;
 
-import static java.util.Objects.requireNonNull;
-
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Optional;
 
 public class Pbkdf2PasswordEncoder implements PasswordEncoder {
 
@@ -14,21 +13,22 @@ public class Pbkdf2PasswordEncoder implements PasswordEncoder {
     private static final int DEFAULT_ENCODE_KEY_LENGTH = 128;
 
     @Override
-    public String encodePassword(String password) {
-        return new String(encodePassword0(requireNonNull(password)));
-    }
-
-    private byte[] encodePassword0(String password) {
-        final var spec = new PBEKeySpec(password.toCharArray(), generateSalt(), DEFAULT_ENCODE_ITERATIONS, DEFAULT_ENCODE_KEY_LENGTH);
+    public Optional<EncodeResult> encodePassword(String password) {
+        final var salt = generateSalt();
+        final var spec = new PBEKeySpec(password.toCharArray(), salt, DEFAULT_ENCODE_ITERATIONS, DEFAULT_ENCODE_KEY_LENGTH);
 
         try {
             final var factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            return factory.generateSecret(spec).getEncoded();
+            final var encoded = factory.generateSecret(spec).getEncoded();
+
+            final var result = new EncodeResult(new String(encoded), salt);
+
+            return Optional.of(result);
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
             e.printStackTrace();
         }
 
-        return new byte[0];
+        return Optional.empty();
     }
 
     private byte[] generateSalt() {
