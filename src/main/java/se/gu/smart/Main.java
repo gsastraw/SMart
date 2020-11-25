b95ce463d8d28e654e57b2c4d1ea9231d5675f54
Mislav Milicevic <mislav.milicevic22@gmail.com>
2020-11-25 23:38:45 +0100

    Add login panel

--- src/main/java/se/gu/smart/Main.java
+++ src/main/java/se/gu/smart/Main.java
@@ -1,7 +1,29 @@
 package se.gu.smart;
 
-public class Main {
-    public static void main(String[] args) {
+import static java.util.Objects.requireNonNull;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        final var classLoader = getClass().getClassLoader();
+
+        final var login = FXMLLoader.load(requireNonNull(classLoader.getResource("fxml/login.fxml")));
+
+        final var scene = new Scene((Parent) login);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
