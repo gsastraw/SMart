e8d79bfb83b6c56bb3e59d2b53389be3de378c93
eyass <meyasshaj@gmail.com>
2020-12-03 01:04:05 +0100

    project issues close and open
    project issues can now be closed and opened

--- src/main/java/se/gu/smart/model/ProjectIssue.java
+++ src/main/java/se/gu/smart/model/ProjectIssue.java
@@ -5,11 +5,13 @@ public class ProjectIssue {
     private int issueNumber;
     private String issueType;
     private String issueName;
+    private boolean issueStatus;
 
-    public ProjectIssue(int issueNumber, String issueType, String issueName){
+    public ProjectIssue(int issueNumber, String issueType, String issueName, boolean issueStatus){
         this.issueNumber = issueNumber;
         this.issueType = issueType;
         this.issueName = issueName;
+        this.issueStatus = true;  //true means open, false means closed.
     }
 
     public int getIssueNumber() {
@@ -22,6 +24,14 @@ public class ProjectIssue {
 
     public String getIssueType() {
         return issueType;
+    }
+
+    public boolean getIssueStatus(){
+        return issueStatus;
+    }
+
+    public void setIssueStatus(boolean issueStatus) {
+        this.issueStatus = issueStatus;
     }
 
     public void setIssueNumber(int issueNumber){
