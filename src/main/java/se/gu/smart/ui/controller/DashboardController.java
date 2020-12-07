a927a4718b73a58478fc1fc0149adc39139f9e12
Edvin <edvin.33d@gmail.com>
2020-12-07 05:03:32 +0100

    Implement Messages Button Redirect

--- src/main/java/se/gu/smart/ui/controller/DashboardController.java
+++ src/main/java/se/gu/smart/ui/controller/DashboardController.java
@@ -1,24 +1,27 @@
 package se.gu.smart.ui.controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import se.gu.smart.ui.util.FXMLUtil;
 
 public final class DashboardController {
 
-    @FXML private Button viewProfileButton;
+    @FXML
+    private Button messagesButton;
 
-    public void viewProfile(ActionEvent event) {
-        var viewProfileParent = FXMLUtil.loadFxml("viewProfile");
-        var viewProfileScene = new Scene((Parent) viewProfileParent);
+    @FXML
+    void redirectMessages(MouseEvent event) {
+        var dashboardParent = FXMLUtil.loadFxml("messages");
+        var dashboardScene = new Scene((Parent) dashboardParent);
 
         var window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(viewProfileScene);
+        window.setScene(dashboardScene);
+        window.centerOnScreen();
         window.show();
     }
 
