d73c0fbafd9be04f0f1a9a7a1455e1b188a98bb8
Edvin <edvin.33d@gmail.com>
2020-12-07 16:27:39 +0100

    View Profile can access other buttons

--- src/main/java/se/gu/smart/ui/controller/ViewProfileController.java
+++ src/main/java/se/gu/smart/ui/controller/ViewProfileController.java
@@ -1,8 +1,40 @@
 package se.gu.smart.ui.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 
-public class ViewProfileController {
+public final class ViewProfileController extends BaseController {
+
+    @FXML
+    void redirectMessages(MouseEvent event) {
+        redirect(event, "user_messages");
+    }
+
+    @FXML
+    void redirectNewProject(MouseEvent event) {
+        redirect(event, "user_new_project");
+    }
+
+    @FXML
+    void redirectCalender(MouseEvent event) {
+        redirect(event, "user_calender");
+    }
+
+    @FXML
+    void redirectProjects(MouseEvent event) {
+        redirect(event, "login");
+    }
+
+    @FXML
+    void redirectDashboard(MouseEvent event) {
+        redirect(event, "user_dashboard");
+    }
+
+    @FXML
+    void redirectTimesheet(MouseEvent event) {
+        redirect(event, "user_timesheet");
+    }
+
     @FXML
     public void initialize() {
     }
