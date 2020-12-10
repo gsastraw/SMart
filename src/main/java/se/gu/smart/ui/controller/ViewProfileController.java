d7134210af5d4b7eade5f52ec45fd08b804d93b5
Klara <klarasve@student.chalmers.se>
2020-12-10 14:39:07 +0100

    Added UI for Timesheet menu and the Timesheet itself. + Some basic stuff in ViewProfileController.

--- src/main/java/se/gu/smart/ui/controller/ViewProfileController.java
+++ src/main/java/se/gu/smart/ui/controller/ViewProfileController.java
@@ -1,4 +1,26 @@
 package se.gu.smart.ui.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+import se.gu.smart.model.UserAccount;
+
 public final class ViewProfileController extends BaseUserController {
+
+    private UserAccount loggedUser;
+
+    @FXML private Text usernameText;
+    @FXML private Text firstNameText;
+    @FXML private Text lastNameText;
+    @FXML private Text ageText;
+    @FXML private Text assignedIssuesText;
+    @FXML private TextArea aboutMeTextArea;
+
+    public void initData(UserAccount loggedUser){
+        firstNameText.setText(loggedUser.getDisplayName());
+        lastNameText.setText(loggedUser.getDisplayName());
+
+
+    }
+
 }
