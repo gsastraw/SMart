fab2760482ac7d37072a30d9e167719df500be7f
Klara <klarasve@student.chalmers.se>
2020-12-11 18:32:28 +0100

    More basic stuff for view profile and new scene for edit profile along with new controller.

--- src/main/java/se/gu/smart/ui/controller/ViewProfileController.java
+++ src/main/java/se/gu/smart/ui/controller/ViewProfileController.java
@@ -1,7 +1,9 @@
 package se.gu.smart.ui.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import se.gu.smart.model.UserAccount;
 
@@ -9,18 +11,31 @@ public final class ViewProfileController extends BaseUserController {
 
     private UserAccount loggedUser;
 
-    @FXML private Text usernameText;
-    @FXML private Text firstNameText;
-    @FXML private Text lastNameText;
-    @FXML private Text ageText;
-    @FXML private Text assignedIssuesText;
-    @FXML private TextArea aboutMeTextArea;
+    @FXML
+    private Text usernameText;
+    @FXML
+    private Text displayText;
+    @FXML
+    private Text ageText;
+    @FXML
+    private Text assignedIssuesText;
+    @FXML
+    private TextArea aboutMeTextArea;
+    @FXML
+    private Button editButton;
+    @FXML
+    void redirectEdit(MouseEvent event) {
+        redirect(event, "user_edit_profile");
+    }
 
-    public void initData(UserAccount loggedUser){
-        firstNameText.setText(loggedUser.getDisplayName());
-        lastNameText.setText(loggedUser.getDisplayName());
+    public void userData(UserAccount loggedUser){
+        usernameText.setText(loggedUser.getDisplayName());
+        displayText.setText(loggedUser.getDisplayName());
+        //ageText.setText(loggedUser.getAge);
+        //assignedIssuesText.setText(loggedUser.getIssues);
+        //aboutMeTextArea.setText(loggedUser.getBio);
+        //
 
 
     }
-
 }
