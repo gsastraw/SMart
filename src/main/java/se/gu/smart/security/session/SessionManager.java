c86d89fad6749913fb5f16f876d7d686e58fbd27
Mislav Milicevic <mislav.milicevic22@gmail.com>
2020-12-12 13:07:06 +0100

    Check for account existance when creating session

--- src/main/java/se/gu/smart/security/session/SessionManager.java
+++ src/main/java/se/gu/smart/security/session/SessionManager.java
@@ -2,12 +2,18 @@ package se.gu.smart.security.session;
 
 import static java.util.Objects.requireNonNull;
 
+import se.gu.smart.exception.UserAccountNotFoundException;
+import se.gu.smart.repository.Repositories;
+import se.gu.smart.repository.UserAccountRepository;
+
 import java.util.Optional;
 import java.util.UUID;
 
 public final class SessionManager {
 
     private static final SessionManager INSTANCE = new SessionManager();
+
+    private final UserAccountRepository userAccountRepository = Repositories.getUserAccountRepository();
 
     private Session activeSession;
 
@@ -25,6 +31,12 @@ public final class SessionManager {
             if (activeSession.getUserId().equals(userId)) {
                 return;
             }
+        }
+
+        final var optionalUserAccount = userAccountRepository.getUserAccount(userId);
+
+        if (optionalUserAccount.isEmpty()) {
+            throw new UserAccountNotFoundException(userId);
         }
 
         this.activeSession = new Session(userId);
--- src/test/java/se/gu/smart/security/session/SessionManagerTest.java
+++ src/test/java/se/gu/smart/security/session/SessionManagerTest.java
@@ -2,17 +2,22 @@ package se.gu.smart.security.session;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import se.gu.smart.security.session.SessionManager;
+import se.gu.smart.exception.UserAccountNotFoundException;
+import se.gu.smart.repository.Repositories;
+import se.gu.smart.repository.UserAccountRepository;
 
 import java.util.UUID;
 
 final class SessionManagerTest {
 
     private SessionManager sessionManager;
+
+    private final UserAccountRepository userAccountRepository = Repositories.getUserAccountRepository();
 
     @BeforeEach
     void setup() {
@@ -23,10 +28,15 @@ final class SessionManagerTest {
     void setActiveSession() {
         final var userId = UUID.randomUUID();
 
-        sessionManager.setActiveSession(userId);
+        assertThrows(UserAccountNotFoundException.class, () -> sessionManager.setActiveSession(userId));
+        assertFalse(sessionManager.getActiveSession().isPresent());
+
+        final var userAccount = userAccountRepository.createUserAccount("username", "name");
+
+        sessionManager.setActiveSession(userAccount.getUserId());
 
         assertTrue(sessionManager.getActiveSession().isPresent());
-        assertEquals(userId, sessionManager.getActiveSession().get().getUserId());
+        assertEquals(userAccount.getUserId(), sessionManager.getActiveSession().get().getUserId());
     }
 
     @Test
