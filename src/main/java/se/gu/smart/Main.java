8ca18c79819151cbef1289c5f3dfa4bedd8d9f85
Gregory Sastrawidjaya <70916445+gsastraw@users.noreply.github.com>
2020-12-20 03:32:36 +0100

    Delete calendar

--- src/main/java/se/gu/smart/Main.java
+++ src/main/java/se/gu/smart/Main.java
@@ -1,10 +1,24 @@
 package se.gu.smart;
 
+import se.gu.smart.model.project.Project;
+import se.gu.smart.repository.AccountRepository;
+import se.gu.smart.service.Services;
+import se.gu.smart.service.AccountService;
 import se.gu.smart.ui.GUIStarter;
+
+import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
+        injectDummyUsers();
         GUIStarter.main(args);
+    }
+
+    private static void injectDummyUsers() {
+        AccountService accService = Services.getUserAccountService();
+        accService.createAdministrator("admin", "pass");
+        accService.createUser("user", "pass");
+
     }
 }
